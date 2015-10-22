083b5059021cddae901c0024783426c078e383a6
tom <toea@manning.com>
2015-10-22 15:03:34 -0700

    Added show question button

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -2,11 +2,14 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Bootstrap = require('react-bootstrap');
 
+var Button = Bootstrap.Button;
+
 var Main = React.createClass({
 
   getInitialState: function() {
     return {
-      spaces: []
+      spaces: [],
+      viewedQuestions: []
     };
   },
 
@@ -21,17 +24,41 @@ var Main = React.createClass({
     }.bind(this));
   },
 
+  showQuestion: function() {
+    // give the user a random question
+    // try 100 times to get an unshown question
+    var questionShown = false;
+    for(var i = 0; i < 100; i++) {
+      var randomPosition = Math.floor(Math.random() * this.state.questions.length);
+      if(this.state.viewedQuestions.indexOf(randomPosition) === -1) {
+        // question hasn't been displayed yet
+        // add it to the viewed questions
+        this.setState({'viewedQuestions': this.state.viewedQuestions.concat(randomPosition)});
+        // and show it to the user
+        alert(this.state.questions[randomPosition].question);
+        questionShown = true;
+        break;
+      }
+    }
+    if(!questionShown) {
+      // player has seen all the questions
+      alert("Sorry - no more questions");
+    }
+  },
+
   render: function () {
     return (
       <div>
         <h1>Earth Layers Game</h1>
         <div id="game">
+          <div id="game-piece"></div>
           {this.state.spaces.map(function(space, index) {
             return (
               <div className="space" key={'space-' + index} style={{left: space.x, top: space.y}}></div>
             );
           })}
         </div>
+        <Button onClick={this.showQuestion}>Give me a question!</Button>
       </div>
     );
   }
